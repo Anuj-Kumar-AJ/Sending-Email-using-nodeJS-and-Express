78a289aa7baeac5bb649619d263ae8d2362180e6
ANUJ KUMAR <45460554+R3v3rb3@users.noreply.github.com>
2022-11-17 20:34:55 +0530

    Added the code for Email sender app from nodeJS in index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+// importing express 
+express = require('express');
+//importing nodemailer
+nodemailer = require('nodemailer');
 
-express = require('express')
-
+// creating an instance of express
 app = express();
+
 port = process.env.port || 3000;
 
-app.get('/', (req,res) => {
-    res.send('you are listning to port '+ port );
-})
-app.listen(port , (req,res) => {
-    console.log(`server listning to port `, port);
-})
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: //"User_ID", 
+      pass: //"User_Password", 
+    },
+  });
+
+// send mail with defined transport object
+const message = await transporter.sendMail({
+    from: '"Anuj " <Anujkumar.sk639@gmail.com>', // sender address
+    to: "user, <anujkumaraj639@gmail.com>", // list of receivers
+    subject: "Email from nodeJS server", // Subject line
+    text: "Hi from nodeJS server", // plain text body
+    html: "<b>Hi from nodeJS server</b>", // html body
+  });
+
+console.log("Message sent: %s", message.messageId);
+  
+ 
